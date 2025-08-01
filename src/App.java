3ee741bed21487c9f7e3e3099b7475f3eb4a0a2b
SamJMackie <61442831+SamJMackie@users.noreply.github.com>
2025-08-01 15:20:39 +1000

    Added Generic Class 'Box'; Passed both Circle and Rectangle objects to Box

--- src/App.java
+++ src/App.java
@@ -4,9 +4,22 @@ public class App {
         AbstractShape abstractShape = new Circle(5);
         System.out.println(abstractShape.getName());
         abstractShape.display();
-        
+
         abstractShape = new Rectangle(4, 6);
         System.out.println(abstractShape.getName());
         abstractShape.display();
+
+        Box<AbstractShape> shapeBox = new Box<>();
+        
+        shapeBox.setItem(new Circle(5));
+        AbstractShape boxShape = shapeBox.getItem();
+        System.out.println(boxShape.getName());
+        boxShape.display();
+
+        shapeBox.setItem(new Rectangle(4, 6));
+        boxShape = shapeBox.getItem();
+        System.out.println(boxShape.getName());
+        boxShape.display();
     }
+
 }
